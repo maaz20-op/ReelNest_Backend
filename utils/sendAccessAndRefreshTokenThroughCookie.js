5ddf2adc630892e5e83e8386eb4ba7ded4e8571e
Maaz Jatt <maazcoding54@gmail.com>
2026-07-06 14:22:43 +0500

    added

--- utils/sendAccessAndRefreshTokenThroughCookie.js
+++ utils/sendAccessAndRefreshTokenThroughCookie.js
@@ -7,16 +7,16 @@ function sendAccessAndRefreshTokenThroughCookies(userEmail, res) {
   console.log("trhis my access torekn", accessToken);
   console.log("trhis my refresh torekn", refreshToken);
   res.cookie("accessToken", accessToken, {
-    httpOnly: true, // prevent JS access to cookie (secure) (csrf attxk saved)
-    secure: false, // only HTTPS in productioin
-    sameSite: "none", // or "Strict" to prevent csrf attack it may not send cookie from unauthorized website
+    httpOnly: true,
+    secure: true, // Must be true for SameSite=None to work
+    sameSite: "none", // only HTTPS in productioin // or "Strict" to prevent csrf attack it may not send cookie from unauthorized website
     maxAge: 15 * 60 * 1000, // 15 min cookie age to live in browser
   });
 
   res.cookie("refreshToken", refreshToken, {
-    httpOnly: true, // prevent JS access to cookie (secure)
-    secure: false, // only HTTPS in prod
-    sameSite: "none", // or "Strict" / "None" based on frontend-backend location
+    httpOnly: true,
+    secure: true, // Must be true for SameSite=None to work
+    sameSite: "none", // only HTTPS in prod // or "Strict" / "None" based on frontend-backend location
     maxAge: 24 * 60 * 60 * 1000 * 5, // 5 day in ms
   });
 }
