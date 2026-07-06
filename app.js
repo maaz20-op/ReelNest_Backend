831f4c5f645222a9f5c18eaebaaa5389bdc0ac1b
Maaz Jatt <maazcoding54@gmail.com>
2026-07-06 13:38:18 +0500

    cors fix

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://reel-nest-frontend.vercel.app/"],
     credentials: true, // Allows the backend to receive/send cookies
   }),
 );
